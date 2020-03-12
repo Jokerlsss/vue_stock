340961dc20a2476655127fda42dadb7d305715d0
Jokerls <1152501640@qq.com>
2020-03-12 19:35:47 +0800

    2020-3-12 Finish the opetate history page'

---
+++ src/components/OprateTitle.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <div class="titleDiv">
+      <p>操作记录</p>
+    </div>
+    <div class="filterDiv">
+      <van-dropdown-menu>
+        <van-dropdown-item :value="value1" :options="option1" disabled />
+        <van-dropdown-item :value="value2" :options="option2" />
+        <van-dropdown-item :value="value1" :options="option1" />
+        <van-dropdown-item :value="value2" :options="option2" />
+      </van-dropdown-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      option1: [
+        { text: '月份', value: 0 },
+        { text: '1', value: 1 },
+        { text: '2', value: 2 },
+        { text: '3', value: 3 },
+        { text: '4', value: 4 },
+        { text: '5', value: 5 },
+        { text: '6', value: 6 },
+        { text: '7', value: 7 },
+        { text: '8', value: 8 },
+        { text: '9', value: 9 },
+        { text: '10', value: 10 },
+        { text: '11', value: 11 },
+        { text: '12', value: 12 }
+      ],
+      option2: [
+        { text: '月份', value: 'a' },
+        { text: '好评排序', value: 'b' },
+        { text: '销量排序', value: 'c' }
+      ],
+      value1: 0,
+      value2: 'a'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  font-family: PingFang SC;
+  border-radius: 10rpx 10rpx 0 0;
+  margin-top: 60rpx;
+  display: flex;
+  flex-wrap: wrap;
+  width: 680rpx;
+  height: auto;
+  background-color: #191b2a;
+}
+/* 标题 */
+.titleDiv {
+  height: 100rpx;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+.titleDiv > p {
+  margin-left: 30rpx;
+  font-size: 32rpx;
+  color: #9898a0;
+}
+/* 筛选框 */
+.filterDiv {
+  height: auto;
+  width: 100%;
+  /* background-color: blue; */
+}
+</style>
--- src/pages/oprateHistory/index.vue
+++ src/pages/oprateHistory/index.vue
@@ -1,24 +1,62 @@
 <template>
   <div class="container">
-    <Steps
-      v-for="(oprateItem,index) in stepsList"
-      :key="index"
-      :operateDate="oprateItem.operateDate"
-      :operateDesc="oprateItem.operateDesc"
-    ></Steps>
+    <OprateTitle></OprateTitle>
+    <scroll-view class="scrollBox" scroll-y="true">
+      <div class="Steps">
+        <Steps
+          v-for="(oprateItem,index) in stepsList"
+          :key="index"
+          :operateDate="oprateItem.operateDate"
+          :operateDesc="oprateItem.operateDesc"
+        ></Steps>
+      </div>
+    </scroll-view>
+    <div class="bottomSpace"></div>
   </div>
 </template>
 
 <script>
 import Steps from '@/components/Steps'
+import OprateTitle from '@/components/OprateTitle'
 export default {
   components: {
-    Steps
+    Steps,
+    OprateTitle
   },
   data () {
     return {
       stepsList: [
         {
+          operateDate: '2020-3-11 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        },
+        {
+          operateDate: '2020-3-10 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        },
+        {
+          operateDate: '2020-3-9 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        },
+        {
+          operateDate: '2020-3-8 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        }, {
+          operateDate: '2020-3-11 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        },
+        {
+          operateDate: '2020-3-10 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        },
+        {
+          operateDate: '2020-3-9 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        },
+        {
+          operateDate: '2020-3-8 14:23:21',
+          operateDesc: '加仓 招商中证白酒 5000 元'
+        }, {
           operateDate: '2020-3-11 14:23:21',
           operateDesc: '加仓 招商中证白酒 5000 元'
         },
@@ -51,5 +89,25 @@ page {
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
+}
+.Steps {
+  background-color: #191b2a;
+  padding-top: 30rpx;
+}
+/* 滚动容器 */
+.scrollBox {
+  max-height: 750rpx;
+  width: 680rpx;
+  white-space: nowrap;
+  color: white;
+  background-color: #393942;
+  font-family: PingFang SC;
+}
+/* 从最后一个操作开始，底部留空 */
+.bottomSpace {
+  height: 40rpx;
+  width: 680rpx;
+  border-radius: 0 0 10rpx 10rpx;
+  background-color: #191b2a;
 }
 </style>
